bd582f52df24ddbb819fa1fbf418b26e9354be6f
titimimi220 <78212635+titimimi220@users.noreply.github.com>
2024-01-23 15:22:40 +0300

    last version
    i hope that this work is final

--- src/test/java/org/example/Test200.java
+++ src/test/java/org/example/Test200.java
@@ -1,20 +1,17 @@
 package org.example;
 
 import io.restassured.RestAssured;
+import junit.framework.Assert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.function.Executable;
 import org.testng.annotations.Test;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.not;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 
 public class Test200 {
-
-//    public class User(){
-//        private int id;
-//        private char email;
-//        private char firstname;
-//        private char lastname;
-//        private char avatar;
-//    }
 
     public void test200(String pun, String val) {
 
@@ -42,6 +39,19 @@ public class Test200 {
                 .body(("data." + pun), equalTo(val));
     }
 
+    public void test200(String pun) {
+
+        RestAssured
+                .given()
+                .baseUri("https://reqres.in")
+                .when()
+                .log().all()
+                .get("/api/users/{id}", 2)
+                .then()
+                .log().all()
+                .body(("data." + pun), not(equalTo(null)));
+    }
+
     public void test200() {
 
         RestAssured
@@ -57,16 +67,39 @@ public class Test200 {
 
     }
 
+
     @Test
-    public void test(){
+    public void test1(){
+        test200();
+
+    }
+    @Test
+    public void test2(){
 
         test200();
-        test200("id",  2);
-        test200("email",  "janet.weaver@reqres.in");
-        test200("first_name",  "suss");
-        test200("last_name",  "sussu");
-        test200("avatar",  "susus");
+        assertAll("тесты:",
+                ()->  test200("id",  2),
+                ()-> test200("email",  "janet.weaver@reqres.in"),
+                ()-> test200("first_name",  "suss"),
+                ()-> test200("last_name",  "sussu"),
+                ()-> test200("avatar",  "susus"));
+
+
+    }
+
+    @Test
+    public void test3(){
+
+        test200();
+        assertAll("тесты:",
+                ()->  test200("id"),
+                ()-> test200("email"),
+                ()-> test200("first_name"),
+                ()-> test200("last_name"),
+                ()-> test200("avatar"));
+
 
     }
 
 }
+
